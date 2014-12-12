f811e7bebaa68f7c0c100c84d523fdc9d5ca07f1
Michael Parrish <michael@zooniverse.org>
2014-12-12 17:22:21 -0600

    Object type

--- lib/json/schema_builder/object.rb
+++ lib/json/schema_builder/object.rb
@@ -4,6 +4,20 @@ module JSON
   module SchemaBuilder
     class Object < ContainerEntity
       register :object
+      attribute :required
+      attribute :min_properties
+      attribute :max_properties
+      attribute :properties
+      attribute :additional_properties
+      attribute :pattern_properties
+
+      def initialize(name, opts = { }, &block)
+        self.properties = { }
+        super
+        children.each do |child|
+          self.properties[child.name] = child.as_json
+        end
+      end
     end
   end
 end
--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 RSpec.describe JSON::SchemaBuilder::Object do
+  subject{ described_class }
   it_behaves_like 'a container entity'
+
+  it{ is_expected.to define_attribute :required }
+  it{ is_expected.to define_attribute :min_properties }
+  it{ is_expected.to define_attribute :max_properties }
+  it{ is_expected.to define_attribute :properties }
+  it{ is_expected.to define_attribute :additional_properties }
+  it{ is_expected.to define_attribute :pattern_properties }
+
+  describe '#initialize' do
+    subject do
+      described_class.new 'name' do
+        string :test1
+        string :test2
+      end
+    end
+
+    let(:string){ kind_of JSON::SchemaBuilder::String }
+    its(:children){ is_expected.to include string }
+    its('children.length'){ is_expected.to eql 2 }
+    its(:properties){ is_expected.to eql test1: { }, test2: { } }
+  end
 end
