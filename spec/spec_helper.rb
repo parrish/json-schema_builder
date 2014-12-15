3f345b113281205a4a0fa199acbe0f724b972b26
Michael Parrish <michael@zooniverse.org>
2014-12-15 17:50:06 -0600

    Fix simplecov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
-require 'pry'
+require 'simplecov'
+SimpleCov.configure do
+  add_filter '/spec'
+end
+
+SimpleCov.start
+
 %w(lib spec/support).each do |path|
   Dir["./#{ path }/**/*.rb"].sort.each{ |file| require file }
 end
 
-require 'simplecov'
-SimpleCov.start
-
+require 'pry'
 require 'rspec/its'
 RSpec.configure do |config|
   config.disable_monkey_patching!
