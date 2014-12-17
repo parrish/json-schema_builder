d97bbb1800d46fee218913dfbcca469e599e0f37
Michael Parrish <michael@zooniverse.org>
2014-12-17 12:04:51 -0600

    Syntactically nicer example of context delegation

--- spec/integration/context_delegation_spec.rb
+++ spec/integration/context_delegation_spec.rb
@@ -1,43 +1,47 @@
 require 'spec_helper'
 
 RSpec.describe Examples::ContextDelegation, type: :integration do
-  context 'with an admin' do
-    it_behaves_like 'a builder' do
-      let(:schema_context) do
-        {
-          user: Examples::ContextDelegation::User.new(role: :admin)
-        }
-      end
-
-      let(:expected_json) do
-        {
-          type: :object,
-          additionalProperties: false,
-          properties: {
-            name: { type: :string },
-            role: { type: :string }
+  [:example, :alternate_example].each do |method_name|
+    context 'with an admin' do
+      it_behaves_like 'a builder' do
+        let(:schema_method){ method_name }
+        let(:schema_context) do
+          {
+            user: Examples::ContextDelegation::User.new(role: :admin)
           }
-        }
+        end
+
+        let(:expected_json) do
+          {
+            type: :object,
+            additionalProperties: false,
+            properties: {
+              name: { type: :string },
+              role: { type: :string }
+            }
+          }
+        end
       end
     end
-  end
 
-  context 'with a user' do
-    it_behaves_like 'a builder' do
-      let(:schema_context) do
-        {
-          user: Examples::ContextDelegation::User.new(role: :user)
-        }
-      end
-
-      let(:expected_json) do
-        {
-          type: :object,
-          additionalProperties: false,
-          properties: {
-            name: { type: :string }
+    context 'with a user' do
+      it_behaves_like 'a builder' do
+        let(:schema_method){ method_name }
+        let(:schema_context) do
+          {
+            user: Examples::ContextDelegation::User.new(role: :user)
           }
-        }
+        end
+
+        let(:expected_json) do
+          {
+            type: :object,
+            additionalProperties: false,
+            properties: {
+              name: { type: :string }
+            }
+          }
+        end
       end
     end
   end
--- spec/support/examples/context_delegation.rb
+++ spec/support/examples/context_delegation.rb
@@ -1,12 +1,20 @@
 module Examples
   class ContextDelegation
     include JSON::SchemaBuilder
+    attr_accessor :user
     delegate :admin?, to: :@user
 
     def example
       object additional_properties: false do
         string :name
         string :role if admin?
+      end
+    end
+
+    def alternate_example
+      object additional_properties: false do
+        string :name
+        string :role if user.admin?
       end
     end
 
